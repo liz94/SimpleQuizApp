c2bc4fe8df02d27e91cd599a128e6887b5cdbc8e
liz94 <lizandra_a@hotmail.com>
2019-03-26 10:23:06 +1100

    Initial Commit

---
+++ SimpleQuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleQuizApp
+//
+//  Created by LIZANDRA ALMEIDA on 26/3/19.
+//  Copyright © 2019 LIZANDRA ALMEIDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
